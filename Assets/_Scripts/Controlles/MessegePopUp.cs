7f2a95255a06ec7130539555b6ded04676fe6b0c
dddushesss <37773701+dddushesss@users.noreply.github.com>
2020-11-07 20:48:07 +0300

    Add messegePopUp

---
+++ Assets/_Scripts/Controlles/MessegePopUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MessegePopUp : Singleton<MessegePopUp>
+{
+    public RectTransform popUpBox;
+    public float PopUpTime = 1;
+    public float ShowUpTime = 1;
+    public float popUpRange = 110f;
+
+    [SerializeField] private Text textBox;
+    [SerializeField] private Ease ease;
+
+
+    [EditorButton]
+    public void MessegePlayer(string messege)
+    {
+        textBox.text = messege;
+        popUpBox.DOAnchorPos(new Vector2(0, -popUpRange),PopUpTime).SetEase(ease);
+        popUpBox.DOAnchorPos(new Vector2(0, popUpRange), PopUpTime).SetEase(ease).SetDelay(ShowUpTime);
+    }
+}
