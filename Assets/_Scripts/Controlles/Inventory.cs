62fee96112f485170e247f65837e1768b9855102
SquarePolynomial <kurshin.jr@gmail.com>
2020-11-07 16:11:53 +0300

    Inventory Added

---
+++ Assets/_Scripts/Controlles/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Singleton<Inventory>
+{
+    [SerializeField] private List<InventoryItem> _initialItems = new List<InventoryItem>();
+
+    public readonly List<InventoryItem> CurrentItems = new List<InventoryItem>();
+    public readonly List<InventoryItem> RemovedItems = new List<InventoryItem>();
+
+    public void Add(InventoryItem item)
+    {
+        CurrentItems.Add(item);
+    }
+
+    public void Remove(InventoryItem item)
+    {
+        CurrentItems.Remove(item);
+        RemovedItems.Add(item);
+    }
+}
---
+++ Assets/_Scripts/Model/Inventory/InventoryItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DATA/Inventory/Item", fileName = "InventoryItem")]
+public class InventoryItem : ScriptableObject
+{
+    [SerializeField] private string _itemName;
+
+    public string ItemName => _itemName;
+}
