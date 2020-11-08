e15350240fdb26b9fa00b2f48eaff1899da1bb3f
SquarePolynomial <kurshin.jr@gmail.com>
2020-11-08 14:08:39 +0300

    aaaa

---
+++ Assets/_Scripts/View/VisualEffects/FadeOutOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FadeOutOnStart : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private float _fadeTime;
+
+    void Awake()
+    {
+        _canvasGroup.alpha = 1;
+        _canvasGroup.DOFade(0, _fadeTime);
+    }
+}
