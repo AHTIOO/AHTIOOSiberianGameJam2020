7957742537777d4711c1a0f1844aff21d4c9c3c5
AHTIOO <aht100@yandex.ru>
2020-11-08 05:29:40 +0300

    Door from 2 to cor1 is now open

--- Assets/_Scripts/View/IDActivationManager.cs
+++ Assets/_Scripts/View/IDActivationManager.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class IDActivationManager : Singleton<IDActivationManager>
 {
+    [SerializeField] private Transform _roomsParent;
+
     public void SetObjectActive(string id, bool isActive)
     {
-        foreach (var act in FindObjectsOfType<IDActivator>())
+        foreach (var act in _roomsParent.GetComponentsInChildren<IDActivator>(true))
         {
             if (act.ids.Contains(id))
             {
